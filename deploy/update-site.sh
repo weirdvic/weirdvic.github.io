601bcec90cd7e417b407f5711aca345f682d7c32
weirdvic <56339132+weirdvic@users.noreply.github.com>
2023-08-01 10:40:22 +0300

    Fix search

--- deploy/update-site.sh
+++ deploy/update-site.sh
@@ -9,6 +9,8 @@ cd ~/weirdvic.github.io
 rm -rf public/*
 hugo
 
-#
+# Генерация индекса для работы поиска по страницам
 docker run --rm -v $PWD/public:/tmp tinysearch/cli index.json
 sudo chown -Rv $USER:$USER ./public/wasm_output
+mv -v ./public/wasm_output/*.{js,wasm} ./public/js
+rm -rf ./public/wasm_output
